f6f901301333a201aa0eea86a96812e3c5e941f3
shreyanshc <shreyanshc@lambdatest.com>
2025-06-27 14:01:05 +0530

    Added smartui sample

---
+++ tests/specs/single_test_smartui.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+describe('Google Search Function', () => {
+  it('can find search results', async () => {
+    await browser.url('https://www.google.co.in/');
+    const prompt = await $('[id="L2AGLb"]'); // consent popup is coming for other location which needs to be accepted to proceed
+    if(prompt.elementId)
+      await prompt.click();
+    const input = await $('[name="q"]');
+    await input.setValue('test123');
+    const title = await browser.getTitle();
+    await browser.pause(10000);
+    assert.equal(title, 'Google');
+    browser.execute(`smartui.takeScreenshot=sample-snapshot`)
+  });
+});
